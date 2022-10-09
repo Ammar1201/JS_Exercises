7eba43bbaae2356f2bd5128f8ccd4392a6a55cc4
Ammar1201 <aamour1201@gmail.com>
2022-10-09 19:04:47 +0300

    6.1 done

--- 3.2.js
+++ 3.2.js
@@ -4,13 +4,22 @@ function peopleOnBus(arr) {
   for(let stop of arr) {
     total += stop[0];
     total -= stop[1];
-    if(total < 0) {
-      total = 0;
-    }
+  }
+
+  if(total < 0) {
+    total = 0;
   }
   return total;
 }
 
 const busStops = [[5, 0], [10, 5], [2, 8], [15, 10], [12, 4]];
 
+const testArr =  [[10,0],[9,1],[8,2],[7,3],[6,4],[5,5],[4,6],[3,7],[2,8],[1,9],[0,9]];
+
+const testArr2 =  [[10,0],[9,1],[8,12],[7,3],[6,4],[5,5],[4,6],[3,7],[2,8],[1,9],[0,9]]; // test for total < 0
+
 console.log(peopleOnBus(busStops));
+
+console.log(peopleOnBus(testArr));
+
+console.log(peopleOnBus(testArr2));
--- 6.1.js
+++ 6.1.js
@@ -1,4 +1,16 @@
 
 function accum(str) {
-  
+  let tmp = '';
+  for(let i = 0; i < str.length; i++) {
+    tmp += str[i].toUpperCase();
+    for(let j = 0; j < i; j++) {
+      tmp += str[i].toLowerCase();
+    }
+    if(i !== str.length - 1) {
+      tmp += '-';
+    }
+  }
+  return tmp;
 }
+
+console.log(accum('RqaEzty'));
